c68d04f22d3f386d6ffccf1029b79ede7267d387
Aic21 <andrei_maxim21@yahoo.com>
2022-03-27 20:07:19 +0300

    Add splash screen

---
+++ app/src/main/java/com/CalculMobil/simplenotes/Splash.java
@@ -0,0 +1,27 @@
+package com.CalculMobil.simplenotes;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Splash extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            }
+        },2000);
+    }
+}
